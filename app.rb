5525d9151f8f41ef36fb56d5a110b4d60ec7d2e5
maksmoroz91 <dlkontakta7@gmail.com>
2020-10-16 16:02:02 +0300

    Display tables with user information

--- app.rb
+++ app.rb
@@ -4,14 +4,14 @@ require 'sinatra/reloader'
 require 'sqlite3'
 
 def get_db
-	@db = SQLite3::Database.new 'barbershop.db'
-	@db.results_as_hash = true
-	return @db
+	db = SQLite3::Database.new 'barbershop.db'
+	db.results_as_hash = true
+	return db
 end	
 
 configure do
-	@db = get_db
-	@db.execute 'CREATE TABLE IF NOT EXISTS
+	db = get_db
+	db.execute 'CREATE TABLE IF NOT EXISTS
 		 "Users" 
 		 (
 		 	"Id" INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -54,14 +54,16 @@ post '/visit' do
 		end
 	end					
 
-	@db = get_db
-	@db.execute 'insert into Users ( username, phone, datestamp, barber, color) 
+	db = get_db
+	db.execute 'insert into Users ( username, phone, datestamp, barber, color) 
 		values ( ?,?,?,?,?)', [@username, @phone, @datetime, @barber, @color]
 
 	erb "#{@username}, мы Вас записали!"
 end	
 
 get '/showusers' do
+	db = get_db
+	@results = db.execute 'select*from Users order by id desc'
   	erb :showusers		
 end
 
